ed6c3fff54a77980a1d2e1f94ee56528c1e15dd5
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2025-02-22 14:31:46 +0700

    Added logic for taking turn and switching turns

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /*
     GameBoard
+        state:
+            board
         initialize
             empty the board
         getSquare
@@ -7,6 +9,12 @@
     Player
         selectSquare
     Game
+        state:
+            players
+            board
+            currentPlayer
+            isPlay
+        methods:
         initialize
             set player name
             set player symbol
@@ -15,12 +23,24 @@
         restart
 */
 
-const gameBoard = (function () {
-    const board = [["", "", ""], ["", "", ""], ["", "", ""]];
+const gameBoard = function () {
+    const rows = 3;
+    const columns = 3;
+    const board = []
 
-    const initializeBoard = () => {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
+    for (let i = 0; i < rows; i++) {
+        board.push([]);
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = board[i].length; j < columns; j++) {
+            board[i].push("");
+        }
+    }
+
+    const resetBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
                 board[i][j] = "";
             }
         }
@@ -30,22 +50,65 @@ const gameBoard = (function () {
 
     const getSquare = (row, column) => board[row][column];
 
-    const setSquare = (row, column, symbol) => {
+    const occupySquare = (row, column, symbol) => {
         board[row][column] = symbol;
     };
 
-    return { initializeBoard, getBoard, getSquare, setSquare };
-})();
+    const displayBoard = () => {
+        let boardDisplay = ""
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                boardDisplay += "[" + board[i][j] + "]";
+            }
+            boardDisplay += "\n";
+        }
+
+        console.log(boardDisplay);
+    }
+
+    return { resetBoard, getBoard, getSquare, occupySquare, displayBoard };
+}
 
 function createPlayer(name, symbol) {
-
     const getName = () => name;
 
     const getSymbol = () => symbol;
 
-    const selectSquare = (row, column) => {
-        gameBoard.setSquare(row, column, symbol);
+    return { getName, getSymbol };
+}
+
+const game = (function (selPlayer1 = "Player 1", selPlayer2 = "Player 2") {
+
+    const players = [];
+    const board = gameBoard();
+
+    const player1 = createPlayer(selPlayer1, "O");
+    const player2 = createPlayer(selPlayer2, "X");
+    players.push(player1, player2);
+
+    let currentPlayer = players[0];
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const changeTurn = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     }
 
-    return { getName, getSymbol, selectSquare };
-}
+    const playRound = (row, column) => {
+        console.log(`${getCurrentPlayer().getName()} selects cell (${row}, ${column})`)
+        board.occupySquare(row, column, getCurrentPlayer().getSymbol());
+
+        changeTurn();
+        displayNewRound();
+    }
+
+    const displayNewRound = () => {
+        board.displayBoard();
+        console.log(`Current player: ${getCurrentPlayer().getName()}`);
+    }
+
+    displayNewRound();
+
+    return { playRound, displayNewRound, getCurrentPlayer };
+
+})();
